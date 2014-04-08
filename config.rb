83c6a4019084820912cd4e8d39bfe705e1ed33b7
Eugene Pempel <eugene.pempel@gmail.com>
2014-04-08 14:56:30 +0400

    add public/ folder for production-ready files

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "stylesheets/scss"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "public/stylesheets"
+sass_dir = "public/stylesheets/scss"
+images_dir = "public/images"
+javascripts_dir = "public/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed
