6f094b0667b4ecf708757b2745a91d7193236d66
Eugene Pempel <eugene.pempel@gmail.com>
2014-04-14 18:43:33 +0400

    install grunt-contrib-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
           sassDir: "public/stylesheets",
           cssDir: "public/stylesheets",
           imagesDir: "public/images",
-          outputStyle: "compressed"
+          outputStyle: "compressed",
         }
       }
     },
@@ -19,9 +19,23 @@ module.exports = function(grunt) {
           ]
         }
       }
-    }
+    },
+    watch: {
+      scripts: {
+        files: [
+          "public/stylesheets/**/*.scss",
+          "public/javascripts/**/*.js",
+          "!public/javascripts/**/*.min.js",
+        ],
+        tasks: ["default"],
+        options: {
+          spawn: false,
+        }
+      }
+    },
   });
   grunt.loadNpmTasks("grunt-contrib-compass");
   grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.registerTask("default", ["compass", "uglify"]);
 };
