2e2eaedd9b6b4d7edc28ef2dc595d7234b507e55
Eugene Pempel <eugene.pempel@gmail.com>
2014-04-19 16:05:58 +0400

    define work and test VMs in Vagrantfile

--- Vagrantfile
+++ Vagrantfile
@@ -3,13 +3,30 @@
 Vagrant.configure("2") do |config|
   config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
 
-  config.vm.define "reflective.app" do |app|
-    app.vm.box = "hashicorp/precise64"
-    app.vm.hostname = "reflective"
-    app.vm.network "forwarded_port", guest: 80, host: 8080, auto_correct: true
+  config.vm.define "reflective-work" do |work|
+    work.vm.box = "hashicorp/precise64"
+    work.vm.hostname = "reflective-work"
+    work.vm.network "private_network", ip: "33.33.13.31"
+    work.vm.network "forwarded_port", guest: 80, host: 8081, auto_correct: true
+    work.vm.synced_folder ".", "/var/apps/reflective", nfs: true
+    work.vm.provider "virtualbox" do |virtualbox|
+      virtualbox.customize ["modifyvm", :id, "--name", "reflective-work"]
+      virtualbox.customize ["modifyvm", :id, "--memory", "256"]
+    end
+    work.vm.provision "shell" do |shell|
+      shell.path = "config/shell/work.sh"
+      shell.args = ["/var/apps/reflective", ENV["TIME_ZONE"] || "Europe/Moscow"]
+      shell.privileged = false
+    end
+  end
 
-    app.vm.provider "virtualbox" do |virtualbox|
-      virtualbox.customize ["modifyvm", :id, "--name", "reflective.app"]
+  config.vm.define "reflective-test" do |test|
+    test.vm.box = "hashicorp/precise64"
+    test.vm.hostname = "reflective-test"
+    test.vm.network "private_network", ip: "33.33.13.32"
+    test.vm.network "forwarded_port", guest: 80, host: 8082, auto_correct: true
+    test.vm.provider "virtualbox" do |virtualbox|
+      virtualbox.customize ["modifyvm", :id, "--name", "reflective-test"]
       virtualbox.customize ["modifyvm", :id, "--memory", "256"]
     end
   end
---
+++ config/shell/work.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+APP_PATH=$1
+TIME_ZONE=$2
+RUBY_VERSION=`cat $APP_PATH/.ruby-version`
+
+function puts()
+{
+  echo "==> `hostname`: $1"
+}
+
+#################################################################################
+## Setting up the specified time zone
+#################################################################################
+cat /etc/timezone | grep $TIME_ZONE > /dev/null 2>&1
+if [[ $? -ne 0 ]]
+then
+  puts "Setting up the $TIME_ZONE time zone..."
+  sudo sh -c "echo '$TIME_ZONE' > /etc/timezone"
+  sudo dpkg-reconfigure -f noninteractive tzdata > /dev/null 2>&1
+fi
+
+################################################################################
+## Installing nginx
+################################################################################
+which nginx > /dev/null 2>&1
+if [[ $? -ne 0 ]]
+then
+  puts "Installing nginx..."
+  sudo apt-get update > /dev/null 2>&1
+  sudo apt-get install -y -qq nginx > /dev/null 2>&1
+  update-rc.d nginx defaults > /dev/null 2>&1
+fi
+
+###############################################################################
+## Installing git, make, g++, curl, libssl-dev, apache2-utils
+###############################################################################
+which git > /dev/null 2>&1
+if [[ $? -ne 0 ]]
+then
+  puts "Installing git, make, g++, curl, libssl-dev, apache2-utils..."
+  sudo apt-get update > /dev/null 2>&1
+  sudo apt-get install -y -qq git make g++ curl > /dev/null 2>&1
+  sudo apt-get install -y -qq libssl-dev apache2-utils > /dev/null 2>&1
+fi
+
+
+###############################################################################
+## Installing node.js and npm without sudo
+###############################################################################
+if [[ ! -d "$HOME/opt" ]]
+then
+  puts "Installing node.js and npm without sudo..."
+  git clone https://github.com/joyent/node.git ~/.node > /dev/null 2>&1
+  cd ~/.node
+  mkdir ~/opt
+  export PREFIX=~/opt; ./configure > /dev/null 2>&1
+  make > /dev/null 2>&1
+  make install > /dev/null 2>&1
+  echo 'export PATH="$HOME/opt/bin:$PATH"' >> ~/.bashrc
+fi
+export PATH="$HOME/opt/bin:$PATH"
+
+###############################################################################
+## Installing rbenv, rbenv-binstubs, ruby-build
+###############################################################################
+if [[ ! -d "$HOME/.rbenv" ]]
+then
+  puts "Installing rbenv, rbenv-binstubs, ruby-build..."
+  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv > /dev/null 2>&1
+  mkdir -p ~/.rbenv/plugins
+  cd ~/.rbenv/plugins
+  git clone https://github.com/ianheggie/rbenv-binstubs.git > /dev/null 2>&1
+  git clone https://github.com/sstephenson/ruby-build.git > /dev/null 2>&1
+  git clone https://github.com/sstephenson/rbenv-gem-rehash.git > /dev/null 2>&1
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+fi
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+###############################################################################
+## Installing ruby
+###############################################################################
+cd $HOME
+rbenv versions | grep $RUBY_VERSION > /dev/null
+if [[ $? -ne 0 ]]
+then
+  puts "Installing ruby $RUBY_VERSION..."
+  rbenv install $RUBY_VERSION > /dev/null 2>&1
+fi
+
+###############################################################################
+## Installing bundler and ruby gems
+###############################################################################
+if [[ ! -d "$APP_PATH/.bundle" ]]
+then
+  cd $APP_PATH
+  gem list | grep bundler > /dev/null 2>&1
+  if [[ $? -ne 0 ]]
+  then
+    puts "Installing bundler..."
+    gem install bundler > /dev/null 2>&1
+  fi
+  puts "Installing ruby gems..."
+  bundle install --path .bundle --binstubs .bundle/bin > /dev/null 2>&1
+fi
+
+###############################################################################
+## Installing npm packages
+###############################################################################
+if [[ ! -d "$APP_PATH/node_modules" ]]
+then
+  cd $APP_PATH
+  puts "Installing npm packages..."
+  npm install > /dev/null 2>&1
+fi
+
+###############################################################################
+## Deploying the applicaiton
+###############################################################################
+if [[ ! -d "$APP_PATH/public" ]]
+then
+  cd $APP_PATH
+  puts "Deploying the application..."
+  mina work:deploy
+fi
