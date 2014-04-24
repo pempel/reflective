98eb7949f1a25b2292f269a941a25a7b6424edf5
Eugene Pempel <eugene.pempel@gmail.com>
2014-04-25 01:05:56 +0400

    improve language.js

--- app/javascripts/language.js
+++ app/javascripts/language.js
@@ -1,8 +1,21 @@
+function setLanguage(language) {
+  if (typeof language === "undefined") {
+    if (typeof $.cookie(language) === "undefined") {
+      language = window.navigator.language;
+    } else {
+      language = $.cookie("language");
+    }
+  }
+  $.cookie("language", /ru/.test(language) ? "ru" : "en");
+  $("body").attr("class", $.cookie("language"));
+  $("#" + $.cookie("language")).parent().children().removeClass("active");
+  $("#" + $.cookie("language")).addClass("active");
+}
+
 $(document).ready(function() {
+  setLanguage();
   $("#en, #ru").click(function(event) {
+    setLanguage(event.target.id);
     event.preventDefault();
-    $("body").attr("class", event.target.id);
-    $(this).parent().children().removeClass("active");
-    $(this).addClass("active");
   });
 });
--- app/javascripts/time.js
+++ app/javascripts/time.js
@@ -1,22 +1,23 @@
+function setTime() {
+  var currentDate = new Date();
+  var currentTime = currentDate.getTime();
+  var currentOffset = currentDate.getTimezoneOffset() * 60 * 1000;
+
+  var moscowOffset = 4 * 3600 * 1000;
+  var moscowTime = currentTime + currentOffset + moscowOffset;
+  var moscowDate = new Date(moscowTime);
+
+  var hours = moscowDate.getHours();
+  if (hours < 10) { hours = "0" + hours; }
+
+  var minutes = moscowDate.getMinutes();
+  if (minutes < 10) { minutes = "0" + minutes; }
+
+  document.getElementById("time").innerHTML = hours + ":" + minutes;
+
+  setTimeout(function() { setTime() }, 1000);
+}
+
 $(window).load(function() {
-  function showTime() {
-    var currentDate = new Date();
-    var currentTime = currentDate.getTime();
-    var currentOffset = currentDate.getTimezoneOffset() * 60 * 1000;
-
-    var moscowOffset = 4 * 3600 * 1000;
-    var moscowTime = currentTime + currentOffset + moscowOffset; 
-    var moscowDate = new Date(moscowTime);
-
-    var hours = moscowDate.getHours();
-    if (hours < 10) { hours = "0" + hours; }
-
-    var minutes = moscowDate.getMinutes();
-    if (minutes < 10) { minutes = "0" + minutes; }
-
-    document.getElementById("time").innerHTML = hours + ":" + minutes;
-
-    setTimeout(function() { showTime() }, 1000);
-  }
-  showTime();
+  setTime();
 });
